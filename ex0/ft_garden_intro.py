85f5055a044b75395eaabb2b248f52db15982c5f
Stephanie Maire <stmaire@z1r10p1.42lyon.fr>
2026-01-07 14:43:01 +0100

    first commit ex0

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def main():
+    print("=== Welcome to My Garden ===")
+    name = "Rose"
+    height = "25"
+    age = "30"
+    print(f"Plant: {name}")
+    print(f"Height: {height}cm")
+    print(f"Age: {age} days")
+    print()
+    print("=== End of Program ===")
+
+
+if __name__ == "__main__":
+    main()
