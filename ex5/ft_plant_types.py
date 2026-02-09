0a4f1dd70a251784e25943e6c5125f396bfc89d2
Stephanie Maire <stmaire@z1r10p2.42lyon.fr>
2026-02-09 17:54:42 +0100

    ex0 to 5

--- ex5/ft_plant_types.py
+++ ex5/ft_plant_types.py
@@ -1,67 +1,120 @@
 class Plant:
-    def __init__(self, name, height, age):
+    """Create a blueprint for secure plants."""
+
+    def __init__(self, name: str, height: int, age: int) -> None:
+        """Initialize plant with name, height and age."""
         self.name = name
         self.height = height
         self.age = age
 
+    def execute_action(self) -> None:
+        """Placeholder for specialized actions in subclasses"""
+        pass
+
+    def display_info(self) -> None:
+        """Placeholder for display info in subclasses"""
+        pass
+
+
 class Flower(Plant):
-    def __init__(self, name, height, age, color):
+    """Create a specialized type called flower"""
+
+    def __init__(self, name: str, height: int, age: int, color: str) -> None:
+        """Initialize flower with name, height, age and color."""
         super().__init__(name, height, age)
         self.color = color
-    
-    def bloom(self):
+
+    def bloom(self) -> None:
+        """Give flower ability to bloom"""
         print(f"{self.name} is blooming beautifully!")
 
+    def execute_action(self) -> None:
+        """execute the specific subclass actions."""
+        self.bloom()
+
+    def display_info(self):
+        """Display info about the subclass"""
+        print(f"{self.name} (Flower): "
+              f"{self.height}cm, {self.age} days, "
+              f"{self.color} color")
+
+
 class Tree(Plant):
-    def __init__(self, name, height, age, trunk_diameter):
+    """Create a specialized type called tree"""
+
+    def __init__(self, name: str,
+                 height: int, age: int,
+                 diameter: int) -> None:
+        """Initialize tree with name, height, age and trunk diameter."""
         super().__init__(name, height, age)
-        self.trunk_diameter = trunk_diameter
-    
-    def produce_shade(self, area):
+        self.diameter = diameter
+
+    def produce_shade(self):
+        """Calculate the shade in function of the diameter."""
+        area: int = int(self.diameter * 1.56)
         print(f"{self.name} provides {area} square meters of shade")
 
+    def execute_action(self) -> None:
+        """execute the specific subclass actions."""
+        self.produce_shade()
+
+    def display_info(self) -> None:
+        """Display tree data."""
+        print(f"{self.name} (Tree): {self.height}cm, {self.age} days, "
+              f"{self.diameter}cm diameter")
+
+
 class Vegetable(Plant):
-    def __init__(self, name, height, age, harvest_season):
+    """Create a specialized type called vegetable"""
+
+    def __init__(self, name: str,
+                 height: int, age: int,
+                 harvest_season: str) -> None:
+        """Initialize vegetable with name, height, age and harvest season."""
         super().__init__(name, height, age)
         self.harvest_season = harvest_season
-    
+
     def nourish(self):
+        """Indicate nutritionnal value"""
         print(f"{self.name} is rich in vitamin C")
 
+    def execute_action(self) -> None:
+        """execute the specific subclass actions."""
+        self.nourish()
+
+    def display_info(self) -> None:
+        """Display vegetable data."""
+        print(f"{self.name} (Vegetable): "
+              f"{self.height}cm, {self.age} days, "
+              f"{self.harvest_season} harvest")
+
+
+def factory_plant(data: tuple) -> Plant:
+    """Create a plant from a tuple of data."""
+    plant_class = data[0]
+    args = data[1:]
+    return plant_class(*args)
+
+
 def plant_types():
+    """Demonstrate polymorphism with different plant types."""
     print("=== Garden Plant Types ===")
     print()
-    data_plants = [
+    garden_data = [
         (Flower, "Rose", 25, 30, "red"),
-        (Flower, "Pivoine", 30, 20, "purple"),
+        (Flower, "Peony", 30, 20, "purple"),
         (Tree, "Oak", 500, 1825, 50),
         (Tree, "Poplar", 300, 365, 45),
         (Vegetable, "Tomato", 80, 90, "summer"),
         (Vegetable, "Cucumber", 30, 80, "summer")
     ]
 
-    for data in data_plants:
-        plant_class = data[0]
-        args = data[1:]
-        new_plant = plant_class(*args)
+    for data in garden_data:
+        new_plant: Plant = factory_plant(data)
+        new_plant.display_info()
+        new_plant.execute_action()
+        print()
 
-        if plant_class == Flower:
-            print(f"{new_plant.name} (Flower): {new_plant.height}cm, {new_plant.age} days, {new_plant.color} color")
-            new_plant.bloom()
-            print()
-
-        if plant_class == Tree:
-            print(f"{new_plant.name} (Tree): {new_plant.height}cm, {new_plant.age} days, {new_plant.trunk_diameter}cm diameter")
-            new_plant.produce_shade(78)
-            print()
-    
-        if plant_class == Vegetable:
-            print(f"{new_plant.name} (Vegetable): {new_plant.height}cm, {new_plant.age} days, {new_plant.harvest_season} harvest")
-            new_plant.nourish()
-            print()
 
 if __name__ == "__main__":
     plant_types()
-
-
-        
